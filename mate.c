ecd8f61ed657a538fe7e502f74784700a9698239
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-03-30 22:12:37 -0600

    Create mate.c

---
+++ mate.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x = 2.0;
+    double y = 3.0;
+
+    double result1 = pow(x, y); // x elevado a la potencia de y
+    double result2 = sqrt(x);   // raíz cuadrada de x
+
+    printf("Resultado 1: %f\n", result1);
+    printf("Resultado 2: %f\n", result2);
+
+    return 0;
+}
